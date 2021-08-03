f741e8db9fb843870c59ef99132025a946673ff5
wanru <12712211548@qq.com>
2021-08-03 12:28:50 +0800

    to loss ??

---
+++ python/oneflow/framework/docstr/dropout.py
@@ -0,0 +1,33 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import oneflow
+from oneflow.framework.docstr.utils import add_docstr
+
+add_docstr(
+    oneflow.F.dropout,  
+    r"""
+    dropout(x: Tensor, *, Float p, Generator generator=None) -> Tensor 
+
+    During training, randomly zeroes some of the elements of the input
+    tensor with probability :attr:`p` using samples from a Bernoulli
+    distribution. Each channel will be zeroed out independently on every forward
+    call.
+    
+    See
+    :class:`~oneflow.nn.Dropout` for more details.
+ 
+    """
+)
