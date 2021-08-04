85c15f0983a33600a8076753923e71c317724bfb
tsai <jackalcooper@gmail.com>
2021-08-04 13:41:01 +0800

    fix CLA

---
+++ python/oneflow/framework/docstr/conv.py
@@ -0,0 +1,57 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import oneflow
+from oneflow.framework.docstr.utils import add_docstr
+
+add_docstr(
+    oneflow.F.conv1d,
+    r"""
+    conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1) -> Tensor
+
+    Applies a 1D convolution over an input signal composed of several input
+    planes.
+
+
+    See :class:`~oneflow.nn.Conv1d` for details and output shape.
+
+        """,
+)
+add_docstr(
+    oneflow.F.conv2d,
+    r"""
+    conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1) -> Tensor
+
+    Applies a 2D convolution over an input image composed of several input
+    planes.
+
+
+    See :class:`~oneflow.nn.Conv2d` for details and output shape.
+        
+        """,
+)
+add_docstr(
+    oneflow.F.conv3d,
+    r"""
+    conv3d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1) -> Tensor
+
+    Applies a 3D convolution over an input image composed of several input
+    planes.
+
+
+    See :class:`~oneflow.nn.Conv3d` for details and output shape.
+        
+        """,
+)
