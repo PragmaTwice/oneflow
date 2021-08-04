7cfd9487d0a1668f1103691c5e7402007e4543c5
zhaofangyuan98 <1067028911@qq.com>
2021-08-04 20:02:07 +0800

    logical_not_kernel.cpp

--- oneflow/user/kernels/logical_not_kernel.cpp
+++ oneflow/user/kernels/logical_not_kernel.cpp
@@ -1,21 +1,48 @@
-#include "oneflow/core/framework/framework.h"
+nclude "oneflow/core/framework/framework.h"
 
 namespace oneflow {
+
 namespace {
 
+template <typename T>
+void Logical_not(DeviceCtx *ctx, const int64_t n, const T *x, const T *y) {
+  for (int64_t i = 0; i != n; ++i) {
+    if((x[i]==true)||(x[i]!=0)){
+      y[i]=std::false;
+    }else if((x[i]==false)||x[i]==0){
+      y[i]=true;
+    }
+  }
+}
 
-REGISTER_USER_OP("logical_not")
-    .Input("in")
-    .Output("out")
-    .SetTensorDescInferFn(
-      [](user_op::InferContext *ctx) -> Maybe<void> {
-        *ctx->Shape4ArgNameAndIndex("out", 0) =
-            *ctx->Shape4ArgNameAndIndex("in", 0);
-        *ctx->Dtype4ArgNameAndIndex("out", 0) =
-            *ctx->Dtype4ArgNameAndIndex("in", 0);
-        return Maybe<void>::Ok();
-      });
-   
+template <DeviceType device_type, typename T>
+class Logical_not_Kernel final : public user_op::OpKernel {
+public:
+  Logical_not_Kernel() = default;
+  ~Logical_not_Kernel() = default;
 
-}  // namespace
-}  // namespace oneflow
+private:
+  void Compute(user_op::KernelComputeContext *ctx) const override {
+    const user_op::Tensor *in_tensor = ctx->Tensor4ArgNameAndIndex("in", 0);
+    user_op::Tensor *out_tensor = ctx->Tensor4ArgNameAndIndex("out", 0);
+    Logical_not<T>(ctx->device_ctx(),
+           in_tensor->shape().elem_cnt(),
+           in_tensor->dptr<T>(),
+           out_tensor->mut_dptr<bool>());
+  }
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
+};
+
+#define REGISTER_RELU_KERNEL(device, dtype)          \
+  REGISTER_USER_KERNEL("logical_not")                     \
+      .SetCreateFn<Logical_not_Kernel<device, dtype>>()      \
+      .SetIsMatchedHob(                              \
+          (user_op::HobDeviceTag() == device) &     \
+          (user_op::HobDataType("out", 0)            \
+            == GetDataType<dtype>::value));
+
+REGISTER_RELU_KERNEL(DeviceType::kCPU, bool)
+REGISTER_RELU_KERNEL(DeviceType::kGPU, bool)
+} // namespace
+
+} // namespace oneflow
